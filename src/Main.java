f2c077f06d03a8913fd4f81562a34e6a71e5b260
Roman Artemev <66all@mail.ru>
2025-09-11 22:52:31 +0300

    Second commit !!!

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,6 @@ public class Main {
         Subtask subtask5 = new Subtask(2, "Организовать стол", "В элитном ресторане");
         Subtask subtask6 = new Subtask(2, "Жене сказать, чтобы купила букет цветов", "Самый большой букет в магазине");
 
-        /*
         System.out.println(taskManager.addTask(task1));
         System.out.println(taskManager.addTask(task2));
         System.out.println(taskManager.addTask(task3));
@@ -39,9 +38,6 @@ public class Main {
         taskManager.removeAllTasks();
         System.out.println(taskManager.getTasks());
 
-
-         */
-
         System.out.println(taskManager.addEpic(epic1));
         System.out.println(taskManager.addEpic(epic2));
 
@@ -55,30 +51,17 @@ public class Main {
         System.out.println(taskManager.getAllEpics());
         System.out.println(taskManager.getAllSubtask());
 
-        /*
-        taskManager.printAllEpics();
-        //taskManager.removeEpicByID(3);
+        taskManager.getEpicById(1);
+        taskManager.getSubtaskById(2);
+
         taskManager.updateEpic(epic1);
-        taskManager.printAllEpics();
-        taskManager.printEpicById(1);
+        taskManager.updateSubtask(subtask3);
+
+        taskManager.removeEpicById(2);
+        taskManager.removeSubtaskById(4);
+
         taskManager.removeAllEpics();
-
-         */
-
-        /*
-        Subtask subtask1 = new Subtask("Пожарить яищницу", "2 яйца пожарить на сковородке в течении 5 минут", Status.NEW);
-        Subtask subtask2 = new Subtask("Попить кофе", "Засыпать кофе в кружку и залить горячей водой, перемешать", Status.NEW);
-        Subtask subtask3 = new Subtask("Пропылесосить", "Вставить шнур от пылесоса в розетку", Status.NEW);
-        Subtask subtask4 = new Subtask("Протереть пыль", "Вытирать сухой тряпкой", Status.NEW);
-
-        taskManager.addSubtask(epic1, subtask1);
-        taskManager.addSubtask(epic1, subtask2);
-        taskManager.addSubtask(epic2, subtask3);
-        taskManager.addSubtask(epic2, subtask4);
-
-        taskManager.printAllSubtasks();
         taskManager.removeAllSubtasks();
 
-         */
     }
 }
